1ef30750fa4ac093656cb04e513fa6ef5680b89d
duyanh1762 <deadpool692.0@gmail.com>
2024-11-08 10:23:01 +0700

    add landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { OrderComponent } from './order/order.component';
+import { LandingComponent } from './landing/landing.component';
 
 const routes: Routes = [
+  {path:"",component:LandingComponent},
   {path:"order",component:OrderComponent},
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OrderComponent } from './order/order.component';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OrderComponent
+    OrderComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
