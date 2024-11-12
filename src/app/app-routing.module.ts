c3fd026dffd8da15f355ea9351399a0d1408f218
duyanh1762 <deadpool692.0@gmail.com>
2024-11-12 10:19:28 +0700

    add login , cart layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { OrderComponent } from './order/order.component';
 import { LandingComponent } from './landing/landing.component';
+import { CartComponent } from './cart/cart.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {path:"",component:LandingComponent},
+  {path:"login",component:LoginComponent},
   {path:"order",component:OrderComponent},
+  {path:"cart",component:CartComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OrderComponent } from './order/order.component';
 import { LandingComponent } from './landing/landing.component';
+import { CartComponent } from './cart/cart.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     OrderComponent,
-    LandingComponent
+    LandingComponent,
+    CartComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
